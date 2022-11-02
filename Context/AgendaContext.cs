b24d2fe75a320c26d88ee665c3cc5a83c96e1496
Cleiton Araújo <cltn.info@gmail.com>
2022-11-02 10:23:16 -0300

    CRUD ASP.NET MVC

---
+++ Context/AgendaContext.cs
@@ -0,0 +1,17 @@
+﻿using AspNetDioMe.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetDioMe.Context
+{
+    public class AgendaContext : DbContext
+    {
+       
+        public AgendaContext(DbContextOptions<AgendaContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Contato> Contatos { get; set; }
+
+    }
+}
---
+++ Views/Contato/Deletar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AspNetDioMe.Views.Contato
+{
+    public class DeletarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
